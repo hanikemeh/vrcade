c41005d760a01f92d7a327f6eca1a84123b20fca
Hani Kemeh <45135301+zhhani89@users.noreply.github.com>
2020-08-13 23:11:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var path = require('path');
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : 'Sq330033-',
+	password : 'Sq423454-',
 	database : 'vrcade'
 });
 
